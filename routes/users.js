7b0b4a3d6aaaaeb302c56d3745518d59b2d3a879
akhil <akhilsharma6721@gmail.com>
2023-03-05 16:03:19 +0530

    added validation to users route

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
-// @route   Get api/profile
-// @desc    Test route
+const { body, validationResult } = require('express-validator');
+// @route   Post api/profile
+// @desc    Register route
 // @access  Public
 
-router.get('/', (req, res) => {
-  res.send('Users Route');
-});
+router.post(
+  '/',
+  [
+    body('name', 'Name is required').notEmpty(),
+    body('email', 'please neter a valid email').isEmail(),
+    body('password', 'Please enter a password of length 6 or more').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('Users Route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect Database
 connectDB();
 
+//body parser
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
   res.send('Api Running');
 });
