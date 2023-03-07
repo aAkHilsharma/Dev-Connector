883368a6e19b4c06af235d77a18470e3bc2e9314
akhil <akhilsharma6721@gmail.com>
2023-03-07 17:42:31 +0530

    fixed error

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     let { name, email, password } = req.body;
