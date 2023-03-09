c439e2de1e1081ed2ddbdce267a527d725eb4fc5
akhil <akhilsharma6721@gmail.com>
2023-03-09 14:24:06 +0530

    route to delete profile and user

--- routes/profile.js
+++ routes/profile.js
@@ -139,4 +139,21 @@ router.get('/user/:user_id', async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route   Delete api/profile
+// @desc    Delete profile, user, post
+// @access  Public
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // remove user
+    await User.findByIdAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 module.exports = router;
