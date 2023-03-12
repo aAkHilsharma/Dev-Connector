e55148669735f8695c07ac7d0fcba1e4448deeaf
akhil <akhilsharma6721@gmail.com>
2023-03-12 17:14:55 +0530

    added a comment

--- routes/posts.js
+++ routes/posts.js
@@ -151,4 +151,35 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
+// @route   Post api/post/comment
+// @desc    Add a comment on a post
+// @access  Private
+
+router.post(
+  '/comment/:id',
+  [auth, body('text', 'Text is required').notEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(404).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const comment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      post.comments.unshift(comment);
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
