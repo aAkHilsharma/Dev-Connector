e840faa4d4b6975540dd45bef6f41c80cda2674d
akhil <akhilsharma6721@gmail.com>
2023-03-20 19:50:54 +0530

    added alerts reducer

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Landing />} />
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/register' element={<Register />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/register' element={<Register />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
