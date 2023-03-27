6c796a57cda7b468ff807ec4070038918d86286d
Akhil Sharma <akhilsharma6721@gmail.com>
2023-03-27 18:21:59 +0530

    added profile reducer

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,25 @@
-import PropTypes from 'react';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentUserProfile } from '../../actions/profile';
 
-const Dashboard = () => {
-  return <>Dashboard.js</>;
+const Dashboard = ({ getCurrentUserProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentUserProfile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return <div className='container'>Dashboard</div>;
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentUserProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getCurrentUserProfile })(Dashboard);
