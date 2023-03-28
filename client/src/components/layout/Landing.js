8cf9145812d21224a5670e863ac104e1d6d7a9e9
Akhil Sharma <akhilsharma6721@gmail.com>
2023-03-28 19:03:09 +0530

    added dash links

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Navigate to='/dashboard' />;
+  }
   return (
     <section className='landing'>
       <div className='dark-overlay'>
@@ -24,4 +29,12 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Landing);
