483b1c56a131eaa48c92f2c20a4160af9f816cb3
Akhil Sharma <akhilsharma6721@gmail.com>
2023-03-28 19:06:12 +0530

    dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,13 +2,38 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentUserProfile } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
+import { Link } from 'react-router-dom';
 
-const Dashboard = ({ getCurrentUserProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentUserProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentUserProfile();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <div className='container'>Dashboard</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <section className='container'>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fa fa-user'></i> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <>has</>
+      ) : (
+        <>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </>
+      )}
+    </section>
+  );
 };
 
 Dashboard.propTypes = {
