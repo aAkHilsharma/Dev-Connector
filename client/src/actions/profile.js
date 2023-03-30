10aad3ee5b01ac2309cf0af5ea138604b9678120
Akhil Sharma <akhilsharma6721@gmail.com>
2023-03-30 11:22:42 +0530

    alert bug fix

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { setAlert } from './alert';
 import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 export const getCurrentUserProfile = () => async (dispatch) => {
@@ -29,17 +28,7 @@ export const createProfile =
         type: GET_PROFILE,
         payload: res.data,
       });
-
-      dispatch({
-        type: setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'),
-      });
     } catch (err) {
-      const errors = err.response.data.errors;
-
-      if (errors) {
-        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-      }
-
       dispatch({
         type: PROFILE_ERROR,
         payload: { msg: err.response.statusText, status: err.response.status },
