c24e01cbc05019f077411a2c7ada36999f97f424
Akhil Sharma <akhilsharma6721@gmail.com>
2023-04-01 17:15:07 +0530

    profiles and repos

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,6 +1,8 @@
 import {
   CLEAR_PROFILE,
   GET_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
   PROFILE_ERROR,
   UPDATE_PROFILE,
 } from '../actions/types';
@@ -22,6 +24,18 @@ export default function profile(state = initialState, action) {
       return {
         ...state,
         profile: payload,
+        loading: false,
+      };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false,
       };
     case PROFILE_ERROR:
