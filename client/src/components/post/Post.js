ac2a5d1687146f7ce8caa63621b0d1d357a4d3ae
Akhil Sharma <akhilsharma6721@gmail.com>
2023-04-02 10:09:35 +0530

    add post form

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getPost } from '../../actions/post';
+import { Link, useParams } from 'react-router-dom';
+import PostItem from '../posts/PostItem';
+
+const Post = ({ getPost, post: { post, loading } }) => {
+  const { id } = useParams();
+  useEffect(() => {
+    getPost(id);
+  }, [getPost, id]);
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <section className='container'>
+      <Link to='/posts' className='btn'>
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </section>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
