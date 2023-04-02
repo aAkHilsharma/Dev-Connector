a3156cbcd6d940cf3707ccad925085bb38c708d3
Akhil Sharma <akhilsharma6721@gmail.com>
2023-04-02 10:54:48 +0530

    added spinner

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import DashActions from './DashActions';
 import Experience from './Experience';
 import Education from './Education';
+import Spinner from '../layout/Spinner';
 
 const Dashboard = ({
   getCurrentUserProfile,
@@ -15,33 +16,40 @@ const Dashboard = ({
   useEffect(() => {
     getCurrentUserProfile();
   }, [getCurrentUserProfile]);
-  return (
-    <section className='container'>
-      <h1 className='large text-primary'>Dashboard</h1>
-      <p className='lead'>
-        <i className='fa fa-user'></i> Welcome {user && user.name}
-      </p>
-      {profile !== null ? (
-        <>
-          <DashActions />
-          <Experience experience={profile.experience} />
-          <Education education={profile.education} />
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <>
+      <section className='container'>
+        <h1 className='large text-primary'>Dashboard</h1>
+        <p className='lead'>
+          <i className='fa fa-user'></i> Welcome {user && user.name}
+        </p>
+        {profile !== null ? (
+          <>
+            <DashActions />
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
 
-          <div className='my-2'>
-            <button onClick={() => deleteAccount()} className='btn btn-danger'>
-              <i className='fas fa-user-minus'></i> Delete My Account
-            </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <p>You have not yet setup a profile, please add some info</p>
-          <Link to='/create-profile' className='btn btn-primary my-1'>
-            Create Profile
-          </Link>
-        </>
-      )}
-    </section>
+            <div className='my-2'>
+              <button
+                onClick={() => deleteAccount()}
+                className='btn btn-danger'
+              >
+                <i className='fas fa-user-minus'></i> Delete My Account
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to='/create-profile' className='btn btn-primary my-1'>
+              Create Profile
+            </Link>
+          </>
+        )}
+      </section>
+    </>
   );
 };
 
