012af6049852d848b0df54e323cad2e90e07fbea
Akhil Sharma <akhilsharma6721@gmail.com>
2023-04-05 20:19:51 +0530

    small refactor

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,6 +1,6 @@
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
-import PropTypes from 'prop-types';
 import { deleteExperience } from '../../actions/profile';
 
 const Experience = ({ experience, deleteExperience }) => {
